ce8aca50179a317bb5e06c0fa0a8b4b6d0d3816d
leevi <leevleh@metropolia.fi>
2023-01-25 13:52:28 +0200

    Uusi Tiedosto

---
+++ moduuli5/tehtava4.py
@@ -0,0 +1,10 @@
+kaupungit = []
+
+for luku in range(5):
+    kaupunki = input("Anna kaupunki: ")
+    kaupungit.append(kaupunki)
+print(kaupungit[0])
+print(kaupungit[1])
+print(kaupungit[2])
+print(kaupungit[3])
+print(kaupungit[4])
