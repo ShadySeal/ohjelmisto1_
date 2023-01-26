dd9d62a3497d69bf78324b30d9dd212d28923630
leevi <leevleh@metropolia.fi>
2023-01-26 18:44:10 +0200

    Uusi Tiedosto

--- moduuli6/tehtava6.py
+++ moduuli6/tehtava6.py
@@ -8,16 +8,23 @@ def pizza2(halkaisija2):
     sade = halkaisija2 / 2
     tulos = pi * (sade * sade)
     return round(tulos)
-
 halkaisija1 = float(input("Anna ensimmäisen pizzan halkaisija: "))
+hinta1 = float(input("Anna ensimmäisen pizzan hinta: "))
 halkaisija2 = float(input("Anna toisen pizzan halkaisija: "))
+hinta2 = float(input("Anna toisen pizzan hinta: "))
 tulos1 = pizza1(halkaisija1)
 tulos2 = pizza2(halkaisija2)
-print("Esnimmäinen pizzan hinta:", tulos1, "€")
+hinta1 -= tulos1
+hinta2 -= tulos2
+if hinta1 < 0:
+    hinta1 = -hinta1
+if hinta2 < 0:
+    hinta2 = -hinta2
+print("Ensimmäinen pizzan hinta:", tulos1, "€")
 print("Toisen pizzan hinta:", tulos2, "€")
-if (tulos1 < tulos2):
-    print("Ensimmäinen pizza on halvempi.")
-elif (tulos1 > tulos2):
-    print("Toinen pizza on halvempi.")
-elif (tulos1 == tulos2):
-    print("Molemmat pizzat ovat saman arvoisia.")
+if (hinta1 < hinta2):
+    print("Ensimmäinen pizza on parempi vastine rahalle.")
+elif (hinta1 > hinta2):
+    print("Toinen pizza on parempi vastine rahalle.")
+elif (hinta1 == hinta2):
+    print("Molemmat pizzat ovat yhtä hyviä vastineita rahalle.")
