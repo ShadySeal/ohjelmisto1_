eee45e2bec574f8ea19807057ef4fd1b45bfdb98
leevi <leevleh@metropolia.fi>
2023-01-26 10:36:57 +0200

    Uusi Tiedosto

--- moduuli6/tehtava2.py
+++ moduuli6/tehtava2.py
@@ -1,9 +1,15 @@
 import random
 
+luku = int(input("Anna maksimiluku: "))
+
 def noppa():
-    arvo = 1
-    while arvo < 6:
+    maara = 0
+    while maara < luku:
         arvo = random.randint(1, 6)
         print(arvo)
+        maara = maara + arvo
+        if (maara > luku):
+            maara = maara - arvo
+    print(maara)
     return
 noppa()
