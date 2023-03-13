2f939eef451d0296ee808c694138b90f95e26c75
leevi <leevleh@metropolia.fi>
2023-03-13 15:12:43 +0200

    Uusi Tiedosto

--- ohjelmisto2/moduuli9/tehtava3.py
+++ ohjelmisto2/moduuli9/tehtava3.py
@@ -12,10 +12,12 @@ class Auto:
         if self.tamanhetkinen_nopeus < 0:
             self.tamanhetkinen_nopeus = 0
 
+    def kulje(self, aika):
+        self.kuljettu_matka = self.tamanhetkinen_nopeus * aika
+
 auto = Auto("ABC-123", 142)
 
-auto.nopeus(30)
-auto.nopeus(70)
-auto.nopeus(50)
+auto.nopeus(60)
+auto.kulje(2)
 
-print(auto.rekisteritunnus, auto.huippunopeus, auto.tamanhetkinen_nopeus, auto.kuljettu_matka)
+print(f"Rekisteritunnus: {auto.rekisteritunnus}, huippunopeus: {auto.huippunopeus}, tamänhetkinen nopeus: {auto.tamanhetkinen_nopeus}, kuljettu matka: {auto.kuljettu_matka}")
